070edd3a8f2147e73ec40e037af50abc21e677d1
Matthew Williams <matthew.lloyd.williams@gmail.com>
2019-09-20 09:44:19 +0100

    Add weather and gist shell funcs

--- shell_funcs.zsh
+++ shell_funcs.zsh
@@ -1,3 +1,5 @@
+# This function strips leading '$' signs off of any copy-pasta from the 
+# internet.
 $ () {
     if (( $# == 0 ))
     then echo "$ needs at least one argument.";
@@ -6,4 +8,15 @@ $ () {
 
     echo "Assuming you pasted a command with a leading $... stripping it now."
     $*
+}
+
+# Finds the weather in a given location.
+wttr () {
+  curl v2.wttr.in/$1;
+  return;
+}
+
+# Update vimrc gist
+vrc-up () {
+  gist -u https://gist.github.com/923efc580ffff32fd209fff53a3fc74c ~/.vimrc
 }
