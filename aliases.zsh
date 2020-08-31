9ac884ec03c43a3d3fdef4844b6388f32b61732f
Matthew Williams <email.m@ttwill.uk>
2020-08-31 18:56:38 +0100

    call fdfind instead of fd
    fd wasn't working in a few scenarios. This is more explicit.

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,10 @@ zsh_aliases='/home/matt/.oh-my-zsh/custom/aliases.zsh'
 
 alias v='vim'
 alias vf='vim $(fzf)'
+
 alias nh='telnet nethack.alt.org'
+
+alias py=python3
 alias senv='source venv/bin/activate'
 
 # git aliases
@@ -10,3 +13,6 @@ alias gg='git grep'
 alias ggi='git grep -i'
 ## recent branches
 alias gbrr='git branch --sort=-creatordate'
+
+# fd
+alias fd=fdfind
